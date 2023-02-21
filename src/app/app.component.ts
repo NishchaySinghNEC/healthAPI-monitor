cbd83fb5434f35ddaa4721769d87babfba131d11
NishchaySinghNEC <117649398+NishchaySinghNEC@users.noreply.github.com>
2023-02-21 14:54:51 +0530

    page 2 UI implementation initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'healthAPI-monitor';
+  application = ['Provider portal', 'Public portal', 'Gateway', 'Admin Portal', 'OSRM', 'Keycloak', 'UI provider', 'UI public', 'UI admin'];
+  dataColumn = ['ID', 'Start date', 'End date', 'Time', 'URI', 'Status', 'Request Body', 'Response message', 'Details'];
 }
