6b36cc963623339630b379576ae4f91bef6c3b17
vikrant-pandey <vikrant.pandey@necsws.com>
2023-02-21 16:09:38 +0530

    Added Page 1 table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  status: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Provider Portal', status: 200 },
+  {position: 2, name: 'Public Portal', status: 200},
+  {position: 3, name: 'Gateway', status: 400},
+  {position: 4, name: 'Admin Portal', status: 200},
+  {position: 5, name: 'OSRM', status: 200 },
+  {position: 6, name: 'Keyclok', status: 400 },
+  {position: 7, name: 'UI Provider', status: 400 },
+  {position: 8, name: 'UI Admin', status: 600 },
+  {position: 9, name: 'UI Public', status: 400 }
+];
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +29,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'healthAPI-monitor';
+
+  displayedColumns: string[] = ['position', 'name', 'status'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table'  
+
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule, 
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
