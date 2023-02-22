5a89692d84f183465232d231e93884a3b7388fac
AwaisIsaneNEC <awais.isane@necsws.com>
2023-02-22 15:28:48 +0530

    added constant url file

---
+++ src/app/url-constants.ts
@@ -0,0 +1,11 @@
+export const ENDPOINTS = {
+    'Provider_Portal':{url:'https://provider.housing-dp.dev/health.json'},
+    'Public_Portal' : {url:'https://northshirecc-qa.housing-dp.dev/health.json'},
+    'Gateway':{url:''},
+    'Admin_Portal':{url:''},
+    'OSRM':{url:''},
+    'Key_Cloak':{url:'https://signin.housing-dp.dev/auth/health'},
+    'UI_Provider':{url:'https://northshirecc-qa.housing-dp.dev/health.json'},
+    'UI_Admin':{url:'https://northshirecc-qa.housing-dp.dev/health.json'},
+    'UI Public':{url:''}
+}
