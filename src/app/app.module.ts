c8c5b91d0bf8cf362d7727f2d08f1e98c57bf075
vikrant-pandey <vikrant.pandey@necsws.com>
2023-02-22 15:59:43 +0530

    app module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table';
 
 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+
 import { LogListComponent } from './log-list/log-list.component';
 import { ApplicationListComponent } from './application-list/application-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { TabsComponent } from './tabs/tabs.component';
-import { HeaderComponent } from './header/header.component';
+
 import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
@@ -23,8 +20,7 @@ import { MaterialModule } from './material/material.module';
     ApplicationListComponent,
     NavbarComponent,
     ErrorpageComponent,
-    TabsComponent,
-    HeaderComponent
+    TabsComponent
   ],
   imports: [
     BrowserModule,
