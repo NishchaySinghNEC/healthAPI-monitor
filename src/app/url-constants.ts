c12ca5a666fefea0c45ad898e29bd6aa6dad2383
NishchaySinghNEC <117649398+NishchaySinghNEC@users.noreply.github.com>
2023-02-23 13:09:20 +0530

    dialog box and local storage implementation

--- src/app/url-constants.ts
+++ src/app/url-constants.ts
@@ -1,11 +1,1 @@
-export const ENDPOINTS = {
-    'Provider_Portal':{url:'https://provider.housing-dp.dev/health.json'},
-    'Public_Portal' : {url:'https://northshirecc-qa.housing-dp.dev/health.json'},
-    'Gateway':{url:''},
-    'Admin_Portal':{url:''},
-    'OSRM':{url:''},
-    'Key_Cloak':{url:'https://signin.housing-dp.dev/auth/health'},
-    'UI_Provider':{url:'https://northshirecc-qa.housing-dp.dev/health.json'},
-    'UI_Admin':{url:'https://northshirecc-qa.housing-dp.dev/health.json'},
-    'UI_Public':{url:''}
-}
+export const ENDPOINTS = JSON.parse(localStorage.getItem('ENDPOINTS') || '{}')
