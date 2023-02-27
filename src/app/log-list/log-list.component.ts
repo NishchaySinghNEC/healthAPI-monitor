6172af2ff954c7d02563d2c0708a734565fe62c5
vikrant-pandey <vikrant.pandey@necsws.com>
2023-02-27 12:28:30 +0530

    Added start date validation

--- src/app/log-list/log-list.component.ts
+++ src/app/log-list/log-list.component.ts
@@ -8,7 +8,7 @@ import { ENDPOINTS } from 'src/app/url-constants'
 import { ApiCallsService } from '../api-calls.service';
 import { LogInterface } from '../log-interface';
 import { LogFormInterface } from '../log-form-interface';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 
 @Component({
@@ -37,7 +37,7 @@ export class LogListComponent implements OnInit, AfterViewInit {
     this.logDetails = this.fb.group({
       limit: [null],
       range: this.fb.group({
-        startDate: [null],
+        startDate: [null, Validators.required],
         endDate: [null]
       }),
       requestMethod: [null],
@@ -61,7 +61,8 @@ export class LogListComponent implements OnInit, AfterViewInit {
   openDialog(id: string, createdDate: string) {
     let dialogData: any
     this.show = true
-    let url = `http://localhost:9000/api-access-log/${id}?createdDate=${createdDate}`
+    const date = this.datepipe.transform(createdDate,'yyyy-MM-dd HH:mm:ss.mms')
+    let url = `http://localhost:9000/api-access-log/${id}?createdDate=${date}`
     this.callLogService.callLogDetails(url).subscribe({
       next: (data) => dialogData = data,
       error: () => this.show = false,
@@ -111,6 +112,7 @@ export class LogListComponent implements OnInit, AfterViewInit {
   }
 
   getLogs(){
+    if (!this.logDetails.valid) return;
     this.show = true
     const url: string = this.appendUrlParams()
     this.callLogService.callLog(url).subscribe({
