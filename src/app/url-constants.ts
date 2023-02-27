3ae6b3cd3ed2e7fc0b7d9347c8e6b73f557fc12a
vikrant-pandey <vikrant.pandey@necsws.com>
2023-02-27 17:37:59 +0530

    removed _

--- src/app/url-constants.ts
+++ src/app/url-constants.ts
@@ -1,11 +1,11 @@
 export const ENDPOINTS = [
-    {applicationName:'Provider_Portal',url:'https://api.housing-dp.dev/provider/healthcheck', status:'FAIL',info:''},
-    {applicationName:'Public_Portal' , url:'https://api.housing-dp.dev/public/healthcheck',status:'FAIL',info:''},
+    {applicationName:'Provider Portal',url:'https://api.housing-dp.dev/provider/healthcheck', status:'FAIL',info:''},
+    {applicationName:'Public Portal' , url:'https://api.housing-dp.dev/public/healthcheck',status:'FAIL',info:''},
     {applicationName:'Gateway',url:'',status:'FAIL',info:''},
-    {applicationName:'Admin_Portal',url:'',status:'FAIL',info:''},
+    {applicationName:'Admin Portal',url:'',status:'FAIL',info:''},
     {applicationName:'OSRM',url:'',status:'FAIL',info:''},
-    {applicationName:'Key_Cloak',url:'https://signin.housing-dp.dev/auth/health',status:'FAIL',info:''},
-    {applicationName:'UI_Provider',url:'https://northshirecc-qa.housing-dp.dev/health.json',status:'FAIL',info:''},
-    {applicationName:'UI_Admin',url:'https://northshirecc-qa.housing-dp.dev/health.json',status:'FAIL',info:''},
-    {applicationName:'UI_Public',url:'',status:'FAIL',info:''}
+    {applicationName:'Key Cloak',url:'https://signin.housing-dp.dev/auth/health',status:'FAIL',info:''},
+    {applicationName:'UI Provider',url:'https://northshirecc-qa.housing-dp.dev/health.json',status:'FAIL',info:''},
+    {applicationName:'UI Admin',url:'https://northshirecc-qa.housing-dp.dev/health.json',status:'FAIL',info:''},
+    {applicationName:'UI Public',url:'',status:'FAIL',info:''}
 ]
