8a1dafa9e5fe838df68f0e6c17318d076893d9d2
vikrant-pandey <vikrant.pandey@necsws.com>
2023-02-28 17:51:53 +0530

    Footer fixed

--- src/app/url-constants.ts
+++ src/app/url-constants.ts
@@ -1,7 +1,7 @@
 export const ENDPOINTS = [
     {applicationName:'Provider Portal',url:'https://api.housing-dp.dev/provider/healthcheck', status:'FAIL',info:''},
     {applicationName:'Public Portal' , url:'https://api.housing-dp.dev/public/healthcheck',status:'FAIL',info:''},
-    {applicationName:'Gateway',url:'http://10.146.17.14:3000/api/healthcheck.json',status:'FAIL',info:''},
+    {applicationName:'Gateway',url:'http://10.146.17.14:5000/apibuilderPing.json',status:'FAIL',info:''},
     {applicationName:'Admin Portal',url:' https://api.housing-dp.dev/admin/healthcheck',status:'FAIL',info:''},
     {applicationName:'OSRM',url:'https://nominatim.openstreetmap.org/status.php',status:'FAIL',info:''},
     {applicationName:'Key Cloak',url:'https://signin.housing-dp.dev/auth/health',status:'FAIL',info:''},
